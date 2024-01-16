bdfe997e709d6d8965c8b0a6e655670f829b802a
Amine Jaoui <aminejaoui01@gmail.com>
2024-01-16 14:57:42 +0100

    adding annotations

---
+++ src/main/java/com/jaoui/zuulserver/filters/MyZuulLogFilter.java
@@ -0,0 +1,40 @@
+package com.jaoui.zuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyZuulLogFilter extends ZuulFilter {
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest req = ctx.getRequest();
+
+        log.info("**** MyZuulLogFilter: Request Method : {} Request URL : {} ", req.getMethod(), req.getRequestURL());
+
+        return null;
+    }
+}
